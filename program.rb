39c1b101a07e2b207968f62188828cf6d1d99683
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-12 13:25:31 +0000

    added INSTRUCTION_COUNT constant, imported breed/random functions from old file but now will be tested

--- program.rb
+++ program.rb
@@ -1,4 +1,13 @@
+INSTRUCTION_COUNT = 12
+
 class ProgramCandidate
-  def initialize
+
+  def self.random(len)
+    len.times.map { |_|
+      [rand(0..11), rand(0..15), rand(0..15), rand(0..15), 0]
+    }
+  end
+  def self.breed(p1=[], p2=[])
+    p1.slice(0, p1.size/2) + p2.slice(p2.size/2, p2.size)
   end
 end
