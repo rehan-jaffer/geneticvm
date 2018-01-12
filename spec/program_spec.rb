1a5c4ac0a4b699dcd20242d39e46f97e9cc0ebe0
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-12 13:29:44 +0000

    check generated programs

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -15,8 +15,20 @@ describe ProgramCandidate do
       expect(@p3.class).to eq Array
     end
 
-    it "breeds two functions and creates a valid program" do
+    it "breeds two functions and creates a valid program (instructions)" do
       expect(@p3.map { |i| i[0] }).to all be < INSTRUCTION_COUNT
+    end
+
+    it "breeds two functions and creates a valid program (r0)" do
+      expect(@p3.map { |i| i[1] }).to all be < REGISTER_COUNT
+    end
+
+    it "breeds two functions and creates a valid program (r1)" do
+      expect(@p3.map { |i| i[2] }).to all be < REGISTER_COUNT
+    end
+
+    it "breeds two functions and creates a valid program (r2)" do
+      expect(@p3.map { |i| i[3] }).to all be < REGISTER_COUNT
     end
 
   end
