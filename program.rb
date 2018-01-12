4fd92a661d93a7297c739d39c8afad39a77440c0
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-12 13:28:52 +0000

    random program function now uses constants

--- program.rb
+++ program.rb
@@ -1,10 +1,11 @@
 INSTRUCTION_COUNT = 12
+REGISTER_COUNT = 16
 
 class ProgramCandidate
 
   def self.random(len)
     len.times.map { |_|
-      [rand(0..11), rand(0..15), rand(0..15), rand(0..15), 0]
+      [rand(0..INSTRUCTION_COUNT-1), rand(0..REGISTER_COUNT-1), rand(0..REGISTER_COUNT-1), rand(0..REGISTER_COUNT-1)]
     }
   end
   def self.breed(p1=[], p2=[])
