efdfb506f13f2e4268e1956c87ed57a080ec8fdb
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-13 13:54:28 +0000

    changed some numbers

--- genetic.rb
+++ genetic.rb
@@ -5,18 +5,28 @@ require './rasm'
 class GeneticProgram
   def self.random(len)
     len.times.map { |_|
-      [rand(0..11), rand(0..2), rand(0..2), rand(0..2)]
+      [rand(0..11), rand(0..15), rand(0..15), rand(0..15)]
     }
   end
-  def self.breed(p1=[], p2=[])
-    (p1 + p2).shuffle.take(p1.size)
+  def self.breed(p1, p2)
 #    p1.slice(0, p1.size/2) + p2.slice(p2.size/2, p2.size)
+   c = true
+   if p1.size > p2.size
+     p2, p1 = p1, p2
+   end
+   p1.zip(p1.map.with_index { |_,i| p2[i] }).map.with_index { |c1, i|
+        c = !c if i % 4 == 0
+        (c == true) ? c1[0] : c1[1]
+     }
+#  p3 = p1.concat(p2)
+#  p3.shuffle.take(p3.size/2)
+
   end
   def self.mutate(p1)
     c = 0
     p1.map do |g|
-      if rand(0..100) == 2
-        g[rand(0..3)] += (rand(1..2) - 1)
+      if rand(0..200) == 2
+        g[rand(0..3)] += (rand(1..4) - 2)
         g
       end
       g
@@ -24,9 +34,10 @@ class GeneticProgram
   end
 end
 
-first_gen = 100.times.map { |_| GeneticProgram.random(rand(10)) }
-inputs = (1..3).to_a
-outputs = inputs.map { |x| ((x**4)).to_f }
+first_gen = 100.times.map { |_| GeneticProgram.random(rand(20)) }
+inputs = (1..5).to_a
+outputs = inputs.map { |x| ((x**2)).to_f }
+first_gen_eval = []
 
 1000.times do |i|
   first_gen_eval = first_gen.map { 
@@ -35,26 +46,32 @@ outputs = inputs.map { |x| ((x**4)).to_f }
    [g, inputs.map.with_index { |i,n|
      vm = VM.new([i]); 
      vm.load(g);
-     ret = vm.run[0] 
+     ret = vm.run[0];
      (outputs[n] - ret).abs
    }.inject(:+), RASM.disasm(g)]
 
-  }.keep_if { |x| !x[1].nan? }
+  }.keep_if { |x| !x[1].nan? }.sort { |x,y| x[1] <=> y[1] }
 
-  pp first_gen_eval.map { |x| x[1] }
-  sorted = first_gen_eval.sort_by { |x| x[1] }
-
-  winners = sorted.take(10).map { |x| x[0] }
-
-  children = []
-  winners.shuffle.each_cons(2) do |x,y|
-    children.push(GeneticProgram.breed(x, y))
-    children.push(GeneticProgram.breed(y, x))
+  if first_gen_eval.map { |x| x[1] }.any? { |x| x == 0 }
+    break
   end
 
-  new_gen = (20.times.map { |_| GeneticProgram.random(rand(10)) })
+  pp first_gen_eval.first(5).map { |x| x[1] }.inject(:+) / 5
+
+  winners = first_gen_eval.first(15).map { |x| x[0] }
+
+  children = []
+  winners.map.with_index { |g, i|
+    i.upto(14) do |j|
+        children.push(GeneticProgram.breed(g, winners[j]))
+    end
+  }
+
+  pp children.size
+
+  new_gen = (10.times.map { |_| GeneticProgram.random(rand(20)) })
   first_gen = (winners.concat(children).concat(new_gen)).map { |w| GeneticProgram.mutate(w) } 
 
 end
 
-pp first_gen.map { |x| RASM.disasm(x) }
+pp first_gen_eval.sort { |x,y| x[1] <=> y[1] }.reverse
