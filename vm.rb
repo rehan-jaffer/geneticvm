17355b7c9a9dfe223995884d49134d0bed954179
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-14 18:23:40 +0000

    added debug functionality, made mangling optional and possible to enable via flags

--- vm.rb
+++ vm.rb
@@ -4,14 +4,30 @@ DEBUG_MODE = false
 MAGIC_NUMBER = 99.0
 REG_SIZE = 4
 
+MANGLE_UNCHANGED_INPUT = 1000
+
 class UnimplementedOpcode < StandardError
 end
 
 class VM
 
-  def initialize(registers=[])
+  def initialize(registers=[], flags=[])
     initialize_registers(registers)
+    @debug = DEBUG_MODE
+    set_flags(flags)
     @pc = 0
+  end
+
+  def enable_debug!
+    @debug = true
+  end
+
+  def set_flags(flags)
+    @flags = flags
+  end
+
+  def enabled?(flag)
+    (@flags.include?(flag))
   end
 
   def initialize_registers(registers)
@@ -37,12 +53,15 @@ class VM
     ops[9] = -> (op, r1, r2, r3) { @r[r1] = Math.sqrt(@r[r2]); } 
     ops[10] = -> (op, r1, r2, r3) { @r[r1] = Math.sin(@r[r2]) }
     ops[11] = -> (op, r1, r2, r3) { @r[r1] = Math.cos(@r[r2]) }
+    ops[12] = -> (op, r1, r2, r3) { (@r[r2] > @r[r3]) ? @pc += 0 : @pc += 1}
+    ops[13] = -> (op, r1, r2, r3) { (@r[r2] <= @r[r3]) ? @pc += 1 : @pc += 2}
+    ops[14] = -> (op, r1, r2, r3) { (@r[r2]) ? @pc += 1 : @pc += 2}
     ops[n]
   end
 
   def debug(op_code, r1, r2, r3)
 
-    return if DEBUG_MODE == false
+    return if @debug == false
 
     case op_code
       when 0
@@ -67,8 +86,13 @@ class VM
         puts "#{@pc} r#{r1} = SIN(r#{r2})"
       when 11
         puts "#{@pc} r#{r1} = COS(r#{r2})"
+      when 12        
+        puts "IF (R#{r2} > R#{r3})"
+      when 13
+        puts "IF (RJ < RK)"
+      when 14
+        puts "IF (RJ)"
       end
-
   end
 
   def final_instruction?
@@ -129,7 +153,7 @@ class VM
   end
 
   def mangle_r0_if_same!
-    @r[0] = MAGIC_NUMBER if @r0_start == @r0_end
+    @r[0] = MAGIC_NUMBER if @r0_start == @r0_end && enabled?(MANGLE_UNCHANGED_INPUT)
   end
 
   def instr_nil_check!(instr)
