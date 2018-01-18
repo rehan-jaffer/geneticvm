000dd002fe7c96901dce53ba6dbf6c17d0070e7c
Rehan Jaffer <ray@thelondonvandal.com>
2018-01-18 17:46:26 +0000

    new breeding function, more robust

--- program.rb
+++ program.rb
@@ -1,6 +1,6 @@
-INSTRUCTION_COUNT = 16
-REGISTER_COUNT = 24
-MUTATION_FREQ = 60
+INSTRUCTION_COUNT = 15
+REGISTER_COUNT = 16
+MUTATION_FREQ = 80
 
 class Program
 
@@ -29,11 +29,12 @@ class BasicMutator
       if rand(0..MUTATION_FREQ) == 2
         op = rand(0..3)
         if op == 0
-          g[op] = ((rand(-3..3)) + g[op]).abs % (INSTRUCTION_COUNT-1)
+          g[op] = ((rand(-1..1)) + g[op]).abs % (INSTRUCTION_COUNT-1)
         else
-          g[op] = ((rand(-3..3).to_f) + g[op]).abs % (REGISTER_COUNT-1)
+          g[op] = ((rand(-1..1).to_f) + g[op]).abs % (REGISTER_COUNT-1)
         end
       end
+      exit if g.any? { |x| x.nil? }
     end
     return p1
   end
@@ -51,11 +52,15 @@ class ProgramCandidate
   def self.breed(p1, p2)
 #    return p1.slice(0, p1.size/2).concat(p2.slice(p2.size/2, p2.size))
 #   c = true
-   p_size = [p1.size, p2.size].sort[0]
-   c1 = rand(0..p_size)
-   c2 = rand(0..p_size)
-   segment = p2[c1, c2]
-   p3 = p1[0, c1] + p2[c1, c2] + p1[c1, p1.size]
+
+   p_size = [p1.size, p2.size].sort.first
+
+   p1_c1 = rand(0..p_size)
+   p1_c2 = rand(p1_c1..p_size)
+
+
+   p3 = p1[0, p1_c1] + p2[p1_c1, (p1_c2-p1_c1)] + p1[p1_c2, (p1.size-p1_c1)]
+
    p3
 #  p3 = p1.concat(p2)
 #  p3.shuffle.take(p3.size/2)
